679e64b60be4c5f4ad9060244bb46f973a74ae86
NikolajGrom <solonliapota@gmail.com>
2021-10-18 22:29:20 +0300

    coomit

--- Program.cs
+++ Program.cs
@@ -18,9 +18,9 @@ namespace LessonFish
                 Name = " Aquarium Big "
             };
 
-            aquarium.Add(new BigFish() { NameFish="carp1", SizeFish = 0.8F, Colour ="grin" });
+            aquarium.Add(new BigFish() { NameFish ="carp1", SizeFish = 0.8F, Colour ="grin" });
             aquarium.Add(new BigFish() { NameFish = "carp2", SizeFish = 0.9F, Colour = "grin" });
-            aquarium.Add(new SmallFish() { NameFish="guppi", SizeFish = 0.2F, Colour= "red", });
+            aquarium.Add(new SmallFish() { NameFish ="guppi", SizeFish = 0.2F, Colour= "red", });
             aquarium.Add(new AverageFish() { NameFish = "gold_fish", SizeFish = 0.6F, Colour = "red-gold" });
 
             Console.WriteLine(aquarium.Name);
